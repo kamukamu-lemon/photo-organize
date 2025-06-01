84ff60903316357efd11ab56fa7cf7f4da782e9f
kamukamu-lemon <megumiinoue.1210@gmail.com>
2025-06-01 15:21:58 +0900

    Basic認証の設定

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
 
   def index
     @photos = Photo.all.order(created_at: :desc)
@@ -19,11 +19,9 @@ before_action :set_photo, only: [:show, :edit, :update, :destroy]
   end
 
   def show
-   
   end
 
   def edit
-   
   end
 
   def update
@@ -42,11 +40,10 @@ before_action :set_photo, only: [:show, :edit, :update, :destroy]
   private
 
   def photo_params
-    params.require(:photo).permit(:title, :memo, {images: []}).merge(user_id: current_user.id)
+    params.require(:photo).permit(:title, :memo, { images: [] }).merge(user_id: current_user.id)
   end
 
   def set_photo
-    @photo =  Photo.find(params[:id])
+    @photo = Photo.find(params[:id])
   end
-
 end
