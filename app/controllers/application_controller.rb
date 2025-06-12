3dfefa5d924260e44170910d7daec69eca5c7289
kamukamu-lemon <megumiinoue.1210@gmail.com>
2025-06-12 20:50:40 +0900

    検索機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :authenticate_user!, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
   def after_sign_in_path_for(_resource)
     photos_path
@@ -17,5 +18,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def set_search
+    @q = Photo.ransack(params[:q])
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,19 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "preview"
+
+document.addEventListener("turbo:load", () => {
+  const openBtn = document.getElementById("open-search");
+  const closeBtn = document.getElementById("close-search");
+  const modal = document.getElementById("search-modal");
+
+  if (openBtn && closeBtn && modal) {
+    openBtn.addEventListener("click", () => {
+      modal.classList.remove("hidden");
+    });
+
+    closeBtn.addEventListener("click", () => {
+      modal.classList.add("hidden");
+    });
+  }
+});
